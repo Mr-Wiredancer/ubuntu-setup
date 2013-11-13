0a7e4d28425fb37fd758bceb15735a7568d910c4
mrwiredancer <mrwiredancer@ubuntu.ubuntu-domain>
2013-11-13 18:55:42 +0800

    finished

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#change ubuntu sources to chinese(163, guangdong) server
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+sudo cp sources.list /etc/apt/
+
+#update apt-get
+sudo apt-get update & sudo apt-get upgrade -y
+
+#https://github.com/huacnlee/init.d.git
+cd ~/
+git clone https://github.com/huacnlee/init.d.git
+
+# go into the folder to run ./install_rvm
+cd init.d
+./install_packages
+./install_rvm
+
+cd ~/
+source ~/.bashrc
+rvm install 1.9.3
+rvm gemset create rails3.2
+rvm --default use 1.9.3@rails3.2
+gem install rails -v='3.2.14' --no-rdoc --no-ri # dont install doc to save time...
+
+#install node js
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
