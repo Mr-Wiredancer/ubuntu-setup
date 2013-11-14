d358634296b95e42c54ee421db4f9251d0ad7a1e
vagrant <vagrant@precise32.(none)>
2013-11-14 07:44:10 +0000

    fixed source bug

--- setup.sh
+++ setup.sh
@@ -15,7 +15,10 @@ cd init.d
 ./install_rvm
 
 cd ~/
-source ~/.bashrc
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+
 rvm install 1.9.3
 rvm gemset create rails3.2
 rvm --default use 1.9.3@rails3.2
@@ -23,5 +26,5 @@ gem install rails -v='3.2.14' --no-rdoc --no-ri # dont install doc to save time.
 
 #install node js
 sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install nodejs
